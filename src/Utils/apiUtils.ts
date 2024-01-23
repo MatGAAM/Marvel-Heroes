01ec9e67cd1d00bcb443c8143bf1efa4edcb1bb4
Matheus Gustavo Alves de Araujo Meneses <MatGAAM>
2024-01-23 13:48:27 -0300

    fix publicKey e privateKey viraram variaveis de ambiente

--- src/Utils/apiUtils.ts
+++ src/Utils/apiUtils.ts
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import md5 from 'md5'
-import { Hero } from '../data/heroes';
 
 
 export const permissionsURL = () => {
-    const publicKey = 'e92bc6259a67fc01077703541f43d4d8';
-    const privateKey =  'd483ae3ea6936dd34f44753f2e458c8dd7e73c56';
+    const publicKey = process.env.PUBLIC_KEY;
+    const privateKey = process.env.PUBLIC_KEY;
     
     const generateHash = (ts: string): string => {
       const inputString = ts + privateKey +   publicKey;
